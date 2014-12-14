9146c0583cf26da2df6b8299057d85504ee019ad
Victor Valotto <vvalotto@gmail.com>
2014-12-14 17:45:16 -0300

    SRP - Paquetizacion de Modulos

--- setup.py
+++ setup.py
@@ -7,8 +7,44 @@ setup(
     description='SenialSOLID: Aplicacion del Principio SRP',
     author='VV',
     author_email='vvalotto@gmail.com',
-    packages=['senial_solid'],
     py_modules=['lanzador'],
     entry_points = {'console_scripts' :
                     'lanzador = lanzador:Lanzador.ejecutar'}
 )
+
+setup(
+    name='adquisidor',
+    version='1.0.1',
+    description='Modulo de Adquiscion',
+    author='VV',
+    author_email='vvalotto@gmail.com',
+    packages=['adquisidor'],
+)
+
+setup(
+    name='modelo',
+    version='1.0.0',
+    description='Modulo de Modelo de Dominio',
+    author='VV',
+    author_email='vvalotto@gmail.com',
+    packages=['modelo'],
+)
+
+setup(
+    name='procesador',
+    version='1.0.1',
+    description='Modulo de Procesamiento',
+    author='VV',
+    author_email='vvalotto@gmail.com',
+    packages=['procesador'],
+)
+
+setup(
+    name='visualizador',
+    version='1.0.1',
+    description='Modulo de Visualizacion',
+    author='VV',
+    author_email='vvalotto@gmail.com',
+    packages=['visualizador'],
+)
+
