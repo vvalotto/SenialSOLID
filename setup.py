98813a4a30057ac9bb3ac098e762b42408554654
Victor Valotto <vvalotto@gmail.com>
2014-12-14 18:40:37 -0300

    SRP - Version Liberada

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ from codecs import open
 
 setup(
     name='SenialSOLID',
-    version='2.1.0',
+    version='2.2.0',
     description='SenialSOLID: Aplicacion del Principio SRP',
     author='VV',
     author_email='vvalotto@gmail.com',
     py_modules=['lanzador'],
-    entry_points = {'console_scripts' :
-                    'lanzador = lanzador:Lanzador.ejecutar'}
+    entry_points={'console_scripts': 'lanzador = lanzador:Lanzador.ejecutar'}
 )
 
 setup(
     name='adquisidor',
-    version='1.0.1',
+    version='1.0.0',
     description='Modulo de Adquiscion',
     author='VV',
     author_email='vvalotto@gmail.com',
@@ -32,7 +31,7 @@ setup(
 
 setup(
     name='procesador',
-    version='1.0.1',
+    version='1.0.0',
     description='Modulo de Procesamiento',
     author='VV',
     author_email='vvalotto@gmail.com',
@@ -41,10 +40,9 @@ setup(
 
 setup(
     name='visualizador',
-    version='1.0.1',
+    version='1.0.0',
     description='Modulo de Visualizacion',
     author='VV',
     author_email='vvalotto@gmail.com',
     packages=['visualizador'],
 )
-
