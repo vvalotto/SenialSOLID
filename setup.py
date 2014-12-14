d55bae44797d2fd012a1396bab2ba605c40607c6
Victor Valotto <vvalotto@gmail.com>
2014-12-13 22:49:11 -0300

    SRP - Ajustado y preparado para testing

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from codecs import open
 
 setup(
     name='SenialSOLID',
-    version='2.0.1-dev',
-    description='SenialSOLID: Aplicacion de l Principio SRP',
+    version='2.2.1-dev',
+    description='SenialSOLID: Aplicacion de l Principio SRP - Modularizacion',
     author='VV',
     author_email='vvalotto@gmail.com',
-    packages=['senial_solid'],
+    packages=['adquisidor', 'modelo', 'procesador', 'visualizador'],
     py_modules=['lanzador'],
     entry_points = {'console_scripts' :
                     'lanzador = lanzador:Lanzador.ejecutar'}
