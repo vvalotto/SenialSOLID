edf0617008ed50dcad9fca9873d0777f8dbc4538
Victor Valotto <vvalotto@gmail.com>
2014-12-20 19:10:12 -0300

    SRP 2.0.0 - Version Entregada

--- setup.py
+++ setup.py
@@ -1,48 +1,11 @@
 from setuptools import setup, find_packages
-from codecs import open
 
 setup(
     name='SenialSOLID',
     version='2.2.0',
-    description='SenialSOLID: Aplicacion del Principio SRP',
+    description='SenialSOLID - Paso 3: Aplicacion del Principio SRP - con Modulos separados',
     author='VV',
     author_email='vvalotto@gmail.com',
-    py_modules=['lanzador'],
+    py_modules=find_packages(),
     entry_points={'console_scripts': 'lanzador = lanzador:Lanzador.ejecutar'}
-)
-
-setup(
-    name='adquisidor',
-    version='1.0.0',
-    description='Modulo de Adquiscion',
-    author='VV',
-    author_email='vvalotto@gmail.com',
-    packages=['adquisidor'],
-)
-
-setup(
-    name='modelo',
-    version='1.0.0',
-    description='Modulo de Modelo de Dominio',
-    author='VV',
-    author_email='vvalotto@gmail.com',
-    packages=['modelo'],
-)
-
-setup(
-    name='procesador',
-    version='1.0.0',
-    description='Modulo de Procesamiento',
-    author='VV',
-    author_email='vvalotto@gmail.com',
-    packages=['procesador'],
-)
-
-setup(
-    name='visualizador',
-    version='1.0.0',
-    description='Modulo de Visualizacion',
-    author='VV',
-    author_email='vvalotto@gmail.com',
-    packages=['visualizador'],
 )
