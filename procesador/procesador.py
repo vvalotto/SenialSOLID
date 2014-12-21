754a86e2fa20bd146e3d535405349e9d5d4774dc
Victor Valotto <vvalotto@gmail.com>
2014-12-21 15:57:09 -0300

    OCP - Inicio del Cambio en el Procesador

--- procesador/procesador.py
+++ procesador/procesador.py
@@ -1,31 +1,77 @@
 """
-Define la clase procesador de la senail
+Para OCP
+Se refactoriza la clase de manera de extender otros tipos de
+funciones de procesmiento de datos sin que impacte en los anteriores programas
+o que cambiando solo las clases de alto nivel que pueda "armar" la solucion
 """
-from modelo.senial import Senial
+from abc import ABCMeta, abstractmethod
+from Modelo.senial import *
 
 
-class Procesador(object):
+class BaseProcesador(metaclass=ABCMeta):
     """
-    Constructor: Inicializa la senial que resultara procesada.
+    Clase Abstracta Procesador
     """
     def __init__(self):
+        """
+        Se inicializa con la senial que se va a procesar
+        """
         self._senial_procesada = Senial()
         return
-    
-    def procesar_senial(self, senial):
+
+    @abstractmethod
+    def procesar(self, senial):
         """
-        Metodo que realiza el procesamiento de la senial
-        :param senial: a procesar
+        Método abstracto que se implementara para cada tipo de procesamiento
+        """
+        pass
+
+    def obtener_senial_procesada(self):
+        """
+        Devuelve la señal procesada
+        """
+        return self._senial_procesada
+
+
+class Procesador(BaseProcesador):
+    """
+    Clase Procesador simple
+    """
+    def procesar(self, senial):
+        """
+        Implementa el procesamiento de duplicar el valor se cada valor de senial
+        :param senial:
         :return:
         """
         print("Procesando...")
-        for i in range(0, senial.obtener_tamanio()):
+        for i in range(0, senial.tamanio):
             self._senial_procesada.poner_valor(senial.obtener_valor(i) * 2)
         return
-    
-    def obtener_senial_procesada(self):
+
+
+class ProcesadorConUmbral(BaseProcesador):
+    """
+    Clase Procesador con Umbral
+    """
+    def __init__(self, umbral):
         """
-        Devuelve la senial procesada
+        Sobreescribe el constructor de la clase abstracta para inicializar el umbral
+        :param umbral:
         :return:
         """
-        return self._senial_procesada
+        BaseProcesador.__init__(self)
+        self._umbral = umbral
+
+    def procesar(self, senial):
+        """
+        Implementa el procesamiento de la senial con umbral
+        :param senial:
+        :return:
+        """
+        print("Procesando con umbral")
+        for i in range(0, senial.tamanio):
+            if senial.obtener_valor(i) < self._umbral:
+                self._senial_procesada.poner_valor(senial.obtener_valor(i))
+            else:
+                self._senial_procesada.poner_valor(0)
+        return
