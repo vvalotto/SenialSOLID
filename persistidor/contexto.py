9a8a91821b8690b7e38a5ac01349ac6d68ac7f78
vvalotto <vvalotto@gmail.com>
2015-01-17 11:29:30 -0300

    PyCharm - MB Air

--- persistidor/contexto.py
+++ persistidor/contexto.py
@@ -4,6 +4,7 @@ en algun tipo de almacen de persistencia (archivo plano, xml, serializa, base de
 """
 import os
 import pickle
+import datetime
 from persistidor.mapeador import *
 
 
@@ -41,6 +42,29 @@ class BaseContexto(metaclass=ABCMeta):
         """
         pass
 
+    def auditar(self, contexto, auditoria):
+        nombre = 'auditor.log'
+        try:
+            with open(nombre, 'a') as auditor:
+                auditor.writelines('------->\n')
+                auditor.writelines(str(contexto) + '\n')
+                auditor.writelines(str(datetime.datetime.now()) + '\n')
+                auditor.writelines(str(auditoria) + '\n')
+        except IOError as eIO:
+            raise eIO
+
+    def trazar(self, contexto, accion, mensaje):
+
+        nombre = 'logger.log'
+        try:
+            with open(nombre, 'a') as logger:
+                logger.writelines('------->\n')
+                logger.writelines('Accion: ' + str(accion) + '\n')
+                logger.writelines('Contexto: ' + str(contexto) + '\n')
+                logger.writelines(str(datetime.datetime.now()) + '\n')
+                logger.writelines(str(mensaje) + '\n')
+        except IOError as eIO:
+            raise eIO
 
 class ContextoPickle(BaseContexto):
     """
@@ -57,6 +81,7 @@ class ContextoPickle(BaseContexto):
             super().__init__(recurso)
             if not os.path.isdir(recurso): os.mkdir(recurso)
         except IOError as eIO:
+            self.trazar("Pickle", "Crear contexto", eIO)
             raise eIO
 
     def persistir(self, entidad, id_entidad):
@@ -108,6 +133,7 @@ class ContextoArchivo(BaseContexto):
             super().__init__(recurso)
             if not os.path.isdir(recurso): os.mkdir(recurso)
         except IOError as eIO:
+            self.trazar("Archivo", "Crear contexto", eIO)
             raise eIO
 
     def persistir(self, entidad, nombre_entidad):
