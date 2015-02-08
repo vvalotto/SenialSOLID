ce6634232224feed1e5ce3df306fc3f27deec326
Victor Valotto <vvalotto@gmail.co>
2015-02-08 16:24:27 -0300

    Templates

--- webapp/flask_main.py
+++ webapp/flask_main.py
@@ -12,5 +12,13 @@ def index():
 def usuario(nombre):
     return render_template('user.html', nombre=nombre)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run()
